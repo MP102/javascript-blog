925280d8afb1519a072d941e2f20cb7e918edae0
Michał Piecyk <michal_piecyk@poczta.fm>
2019-07-13 20:48:41 +0200

    Upgrade script.js Add funcions Lines 8-37

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 'use strict';
 
 function titleClickHandler(event) {
+    event.preventDefault();
     const clickedElement = this;
     console.log('Link was clicked!');
     console.log(event);
@@ -10,7 +11,7 @@ function titleClickHandler(event) {
     for (let activeLink of activeLinks) {
         activeLink.classList.remove('active');
     }
-    /* [IN PROGRESS] add class 'active' to the clicked link */
+    /* [DONE] add class 'active' to the clicked link */
     clickedElement.classList.add('active');
     console.log('clickedElement:', clickedElement);
     /* [DONE] remove class 'active' from all articles */
@@ -20,11 +21,21 @@ function titleClickHandler(event) {
         activeArticle.classList.remove('active');
     }
 
-    /* get 'href' attribute from the clicked link */
+    /* [DONE] get 'href' attribute from the clicked link */
 
-    /* find the correct article using the selector (value of 'href' attribute) */
+   const articleSelector = clickedElement.getAttribute('href');
+   console.log(articleSelector);
+    
 
+    /* [DONE] find the correct article using the selector (value of 'href' attribute) */
+
+    const targetArticle = document.querySelector(articleSelector);
+    console.log(targetArticle);
+  
     /* add class 'active' to the correct article */
+    
+    targetArticle.classList.add('active');
+   
 }
 
 const links = document.querySelectorAll('.titles a');

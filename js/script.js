31776a211870f05480b9cd3f78a0267089a655c7
Michał Piecyk <michal_piecyk@poczta.fm>
2019-07-13 13:55:44 +0200

    Upgrade index.html script.js

--- js/script.js
+++ js/script.js
@@ -1,26 +1,35 @@
 'use strict';
 
-function titleClickHandler(event){
+function titleClickHandler(event) {
     console.log('Link was clicked!');
     console.log(event);
     /* remove class 'active' from all article links  */
-  
+    const activeLinks = document.querySelectorAll('.titles a.active');
+
+    for (let activeLink of activeLinks) {
+        activeLink.classList.remove('active');
+    }
     /* add class 'active' to the clicked link */
-  
+
     /* remove class 'active' from all articles */
-  
+    const activeArticles = document.querySelectorAll('.posts  article.active');
+
+    for (let activeArticle of activeArticles) {
+        activeArticle.classList.remove('active');
+    }
+
     /* get 'href' attribute from the clicked link */
-  
+
     /* find the correct article using the selector (value of 'href' attribute) */
-  
+
     /* add class 'active' to the correct article */
-  }
-  
-  const links = document.querySelectorAll('.titles a');
-  
-  for(let link of links){
+}
+
+const links = document.querySelectorAll('.titles a');
+
+for (let link of links) {
     link.addEventListener('click', titleClickHandler);
-  }
+}
 
 
 
@@ -34,9 +43,9 @@ function titleClickHandler(event){
     const links = document.querySelectorAll('.titles a');
     console.log(links);
   }
-  
+
   const buttonTest = document.getElementById('button-test');
-  
+
   buttonTest.addEventListener('click', titleClickHandler);
   */
 

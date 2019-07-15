6aa8b6cea6b9f9577fdb666390ecaf0c35377f37
Michał Piecyk <michal_piecyk@poczta.fm>
2019-07-15 19:18:06 +0200

    Upgrade script.js - change functions order

--- js/script.js
+++ js/script.js
@@ -38,12 +38,6 @@ function titleClickHandler(event) {
 
 }
 
-const links = document.querySelectorAll('.titles a');
-
-for (let link of links) {
-    link.addEventListener('click', titleClickHandler);
-}
-
 
 const optArticleSelector = '.post',
     optTitleSelector = '.post-title',
@@ -93,4 +87,8 @@ function generateTitleLinks() {
 
 generateTitleLinks();
 
-
+const links = document.querySelectorAll('.titles a');
+console.log(links);
+for (let link of links) {
+    link.addEventListener('click', titleClickHandler);
+}
